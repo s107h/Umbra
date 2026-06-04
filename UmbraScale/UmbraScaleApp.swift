5a7e71b1139d875593ac0d26c6af261f9affd864
Sid <test@test.com>
2026-06-04 17:41:25 -0500

    Initial Commit

---
+++ UmbraScale/UmbraScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbraScaleApp.swift
+//  UmbraScale
+//
+//  Created by Sid McDonald on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct UmbraScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
